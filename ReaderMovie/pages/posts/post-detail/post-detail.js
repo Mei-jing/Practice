76d63b83d9379df8e801a9e49e404319e9aa3813
Merkin <1328751622@qq.com>
2020-03-28 05:02:45 +0800

    Favorites cache.

--- ReaderMovie/pages/posts/post-detail/post-detail.js
+++ ReaderMovie/pages/posts/post-detail/post-detail.js
@@ -1,65 +1,102 @@
+var postsData = require("../../../data/posts-data.js")
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    
+  onLoad: function(options) {
+    var postId = options.id;
+    this.data.currentPostId = postId;
+    console.log(postId);
+    this.setData({
+      postData: postsData.postList[postId] // 此处 postsData 是对象，postsData 下面的 postlist 才是数组，详见 posts-data.js 文件；
+    });
+
+  //初始化
+    var postCollected = wx.getStorageSync('posts_collected')
+    if(postsCollected){
+      var postCollected = postsCollected[postId]
+      this.setData({
+        collected:postCollected  // 收藏状态绑定
+      })
+    }else{
+      var postsCollected = {};
+      postsCollected[postId] = false;
+      wx.setStorageSync('posts_collected', postsCollected)
+    }
+
+   
   },
+
+  onCollectionTap:function(event){
+    var postsCollected= wx.getStorageSync('posts_collected');
+    var postCollected = postsCollected[this.data.currentPostId];
+    postCollected = !postCollected; // 取反，收藏变成未收藏，未收藏变成收藏
+    postsCollected[this.data.currentPostId] = postCollected;
+    // 更新缓存
+    wx.setStorageSync("posts_collected", postsCollected)
+    // 更新数据绑定,从而实现图片切换
+    this.setData({
+      collected:postCollected
+    })
+  },
+
+
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-    
+  onReady: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-    
+  onShow: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-    
+  onHide: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-    
+  onUnload: function() {
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-    
+  onPullDownRefresh: function() {
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-    
+  onReachBottom: function() {
+
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-    
+  onShareAppMessage: function() {
+
   }
 })
--- ReaderMovie/pages/posts/posts.js
+++ ReaderMovie/pages/posts/posts.js
@@ -16,19 +16,64 @@ Page({
    */
   onLoad: function() {
 
-    // this.data.postList = postsData.postList
+    // this.data.postList = postsData.postList 弃用
     this.setData({
       postList: postsData.postList
     });
   },
 
-  onPostTap:function(event){
-    var postId=event.currentTarget.dataset.postid
+  onPostTap: function(event) {
+    var postId = event.currentTarget.dataset.postid
     console.log("on postId is " + postId)
     wx.navigateTo({
-      url: './post-detail/post-detail',
+      url: './post-detail/post-detail?id=' + postId,
+    }) 
+  },
+
+
+  /* 
+ 注意！！！
+ Page({
+    data: {
+      text: 'init data',
+      num: 0,
+      array: [{ text: 'init data'}],
+      object: {
+        text: 'init data'
+    }
+    },
+    changeText: function() {
+    // this.data.text = 'changed data' // 不要直接修改 this.data
+    // 应该使用 setData
+    this.setData({
+      text: 'changed data'
     })
   },
+  changeNum: function () {
+    // 或者，可以修改 this.data 之后马上用 setData 设置一下修改了的字段
+    this.data.num = 1
+    this.setData({
+      num: this.data.num
+    })
+  },
+  changeItemInArray: function () {
+    // 对于对象或数组字段，可以直接修改一个其下的子字段，这样做通常比修改整个对象或数组更好
+    this.setData({
+      'array[0].text': 'changed data'
+    })
+  },
+  changeItemInObject: function () {
+    this.setData({
+      'object.text': 'changed data'
+    });
+  },
+  addNewField: function () {
+    this.setData({
+      'newField.text': 'new data'
+    })
+  }
+})
+*/
 
   /**
    * 生命周期函数--监听页面初次渲染完成
